c1231e7082220aa231ed33ce15258af5d7a7b7e3
alessanderleite <alessander.logistica@gmail.com>
2019-04-24 19:22:04 -0300

    Jackson Dataformat XML add dependency

--- src/main/java/br/com/alessanderleite/app/ws/ui/controller/UserController.java
+++ src/main/java/br/com/alessanderleite/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.alessanderleite.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alessanderleite.app.ws.us.model.response.UserRest;
 
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
@@ -21,9 +24,16 @@ public class UserController {
 		
 	}
 	
-	@GetMapping(path="{userId}")
-	public String getUser(@PathVariable String userId) {
-		return "get user was called with userId = " + userId;
+	@GetMapping(path="{userId}", produces = {
+			MediaType.APPLICATION_XML_VALUE, 
+			MediaType.APPLICATION_JSON_VALUE
+			})
+	public UserRest getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Alessander");
+		returnValue.setLastName("Leite");
+		return returnValue;
 	}
 	
 	@PostMapping
